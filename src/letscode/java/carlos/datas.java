f62e9a60f3dff4c8908ff68c41927ebfa8d9b70e
Carlos <josecarlos.jr.pb@gmail.com>
2023-11-10 23:56:48 -0300

    Comit da aula de datas e saudações

--- src/letscode/java/carlos/datas.java
+++ src/letscode/java/carlos/datas.java
@@ -1,6 +1,7 @@
 package letscode.java.carlos;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
@@ -22,6 +23,21 @@ public class datas {
 		String country = "BR";
 		Locale brasil = new Locale(language, country);
 		System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil));
+		String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+		String saudacao;
+		LocalDateTime agora = LocalDateTime.now();
+		if (agora.getHour() >= 0 && agora.getHour() <12) {
+			saudacao = "Bom dia";
+		} else if (agora.getHour() >= 12 && agora.getHour() <18) {
+			saudacao = "Boa tarde";
+		} else if (agora.getHour() >= 18 && agora.getHour()<24) {
+			saudacao = "Boa noite";
+		} else {
+			saudacao = "Olá";
+		}
+		
+		System.out.printf("Olá, %s. Hoje é %s, %s.%n", nome, diaSemana, saudacao.toUpperCase());
+		
 	}
 
 }
